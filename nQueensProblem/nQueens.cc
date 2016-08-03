43042139ac43acf35d597eec667add84b1a74c13
wscisme <wscisme@gmail.com>
2016-08-03 16:28:59 -0700

    Working solution to the n-Queens problem

--- nQueensProblem/nQueens.cc
+++ nQueensProblem/nQueens.cc
@@ -19,45 +19,58 @@ vector< vector<string> > Solution::solveNQueens(int n) {
   if (n < 1) {
     cout << "Invalid value. Please input with N > 0." << endl;
     return solutions;
+  } else if (n == 1) {
+    solutions.push_back(vector<string>{"Q"});
+  } else if (n < 4) {
+    return solutions;
   }
 
-  int* permute = new int[n];
+  int permute[n];
+  int sums[n];
+  int difs[n];
 
-  if (all_permutes.size() == 0) {
-    cout << "0 possibility found." << endl;
-    return solutions;
+  int row = 0;
+  int col = 0;
+  while (true) {
+    if (col == n) {
+      --row;
+      if (row < 0) break;
+      col = permute[row] + 1;
+      continue;
+    }
+    int sum = row + col;
+    int dif = row - col;
+    bool trynext = false;
+    for (int j = 0; j < row; ++j) {
+      trynext = (permute[j] == col || (sums[j] == sum) || (difs[j] == dif));
+      if (trynext) break;
+    }
+    if (trynext) { ++col; continue; }
+
+    permute[row] = col;
+    sums[row] = sum;
+    difs[row] = dif;
+    ++row;
+    col = 0;
+      
+    if (row == n) {
+      int* goodpermute = new int[n];
+      for (int k = 0; k < n; ++k) goodpermute[k] = permute[k];
+      all_permutes.push_back(goodpermute);
+      row -= 2;
+      col = permute[row] + 1;
+    }
   }
 
   string rowtemplate(n, '.');
   for (auto it = all_permutes.begin(); it != all_permutes.end(); ++it) {
     vector<string> sol;
     for (int i = 0; i < n; ++i)
-      sol.push_back(string(rowtemplate).replace(*it[i], 1, "Q"));
+      sol.push_back(string(rowtemplate).replace((*it)[i], 1, "Q"));
     solutions.push_back(sol);
   }
 
   return solutions;
-}
-
-bool Solution::findPermutation(int* permute, const int n, int j, int* coltaken, int* sums, int* difs) {
-  if (j == 8) {
-    int* goodpermute = new int[n];
-    all_permutes.push_back(permute);
-    return true;
-  }
-
-  //   if (coltaken & 1 << k) continue;
-  //   if (sums & 1 << j << k) continue;
-  //   if (difs & 1 << 7 << j >> k) continue;
-
-  //   int coltaken_new = coltaken | 1 << k;
-  //   int sums_new = sums | 1 << j << k;
-  //   int difs_new = difs | 1 << 7 << j >> k;
-  //   permute[j] = k;
-  //   findPermutation(permute, j+1, coltaken_new, sums_new, difs_new);
-  // }
-
-  return false;
 }
 
 int main()
@@ -73,6 +86,7 @@ int main()
   solution = s.solveNQueens(8);
 
   cout << "The total posibilities for n = " << 8 << " is: " << solution.size() << endl;
+  for (int i = 0; i < 8; ++i) cout << solution[0][i] << endl;
 
   return 0;
 }
