d629c3f7b9699cdcd278a5208c7fedce7bd5a47d
wscisme <wscisme@gmail.com>
2016-08-09 18:39:03 -0700

    Try and come back under development

--- sudoku/sudoku.cc
+++ sudoku/sudoku.cc
@@ -28,8 +28,7 @@ void Solution::solveSudoku(vector<vector<char>>& board) {
   std::fill_n(candblk, 9, ALLNUM);
 
   vector<int*> emptycells;
-  // vector<int*> cellcandbyrow[9];
-  // vector<int*> cellcandbycol[9];
+  vector<int*> poped_at_trial;
 
   // scan the board to narrow down the candidates
   for (int i = 0; i < 9; ++i) {
@@ -40,8 +39,6 @@ void Solution::solveSudoku(vector<vector<char>>& board) {
         cell[1] = j;
         cell[2] = blkpos(i, j);
         emptycells.push_back(cell);
-        // cellcandbyrow[i].push_back(cell + 3);
-        // cellcandbycol[j].push_back(cell + 3);
       } else {
         int thisnum = 1 << (board[i][j] - '0'); // binary rep for the number
         candrow[i] ^= thisnum;
@@ -51,18 +48,18 @@ void Solution::solveSudoku(vector<vector<char>>& board) {
     }
   }
 
-  bool update_cells = false;
+  bool is_trial = false;
   do {
     for (auto it = emptycells.begin(); it != emptycells.end(); ++it) {
       (*it)[3] = candrow[(*it)[0]] & candcol[(*it)[1]] & candblk[(*it)[2]];
-      (*it)[4] = (std::bitset<11>((*it)[3])).count();
+      (*it)[4] = (std::bitset<10>((*it)[3])).count();
     }
     std::sort(emptycells.begin(), emptycells.end(), sortByCount);
     // cout << "A sorting is done." << endl;
 
-    update_cells = false;
+    bool update_cells = false;
     for (auto it = emptycells.begin(); it != emptycells.end(); ++it) {
-      // cout << "At " << (*it)[0] << " " << (*it)[1] << " with (*it)[4] = " << (*it)[4] << endl;
+      cout << "At " << (*it)[0] << " " << (*it)[1] << " with (*it)[4] = " << (*it)[4] << endl;
       if ((*it)[4] == 1) {
         update_cells = true;
         int num = 1;
@@ -71,32 +68,87 @@ void Solution::solveSudoku(vector<vector<char>>& board) {
         candrow[(*it)[0]] ^= (*it)[3];
         candcol[(*it)[1]] ^= (*it)[3];
         candblk[(*it)[2]] ^= (*it)[3];
-        delete [] (*it);
+        if (is_trial) poped_at_trial.push_back(*it);
+        else delete [] (*it);
         emptycells.erase(it--);
-      } else {
-        // cout << "A break is called!\n";
+      }
+      else if ((*it)[4] == 0) {
+        cout << "This trial is damned, going back!" << endl;
+        cout << "Trial size: " << poped_at_trial.size() << endl;
+        for (auto rit = poped_at_trial.rbegin(); rit != poped_at_trial.rend(); ++rit) {
+          if ((*rit)[4] == 1) {
+            emptycells.insert(emptycells.begin(), (*rit));
+            candrow[(*rit)[0]] ^= (*rit)[3];
+            candcol[(*rit)[1]] ^= (*rit)[3];
+            candblk[(*rit)[2]] ^= (*rit)[3];
+            poped_at_trial.pop_back();
+            rit = poped_at_trial.rbegin();
+          } else {
+            candrow[(*rit)[0]] ^= (*rit)[4];
+            candcol[(*rit)[1]] ^= (*rit)[4];
+            candblk[(*rit)[2]] ^= (*rit)[4];
+            (*rit)[4] = (std::bitset<11>((*rit)[3])).count();
+            emptycells.insert(emptycells.begin(), (*rit));
+            poped_at_trial.pop_back();
+            rit = poped_at_trial.rbegin();
+            break;
+          }
+        }
+        if (poped_at_trial.size() == 0) is_trial = false;
+        break;
+      }
+      else if (update_cells) {
+        cout << "Updating cells!\n";
+        break;
+      }
+      else {
+        is_trial = true;
+        int num = 1;
+        while (!((*it)[3] & (1 << num))) ++num;
+        board[(*it)[0]][(*it)[1]] = num + '0';
+        (*it)[4] = (1 << num);  // use the count to store test number
+        candrow[(*it)[0]] ^= (*it)[4];
+        candcol[(*it)[1]] ^= (*it)[4];
+        candblk[(*it)[2]] ^= (*it)[4];
+        (*it)[3] ^= (*it)[4];
+        poped_at_trial.push_back(*it);
+        emptycells.erase(it--);
+        cout << "Updating cells!\n";
         break;
       }
     }
-  } while (update_cells);
+  } while (emptycells.size() > 0);
 
 }
 
 
 int main()
 {
-  vector<vector<char>> board;
-  board.push_back(vector<char>{'5','3','.', '.','7','.', '.','.','.'});
-  board.push_back(vector<char>{'6','.','.', '1','9','5', '.','.','.'});
-  board.push_back(vector<char>{'.','9','8', '.','.','.', '.','6','.'});
+  vector<vector<char>> board1;
+  board1.push_back(vector<char>{'5','3','.', '.','7','.', '.','.','.'});
+  board1.push_back(vector<char>{'6','.','.', '1','9','5', '.','.','.'});
+  board1.push_back(vector<char>{'.','9','8', '.','.','.', '.','6','.'});
 
-  board.push_back(vector<char>{'8','.','.', '.','6','.', '.','.','3'});
-  board.push_back(vector<char>{'4','.','.', '8','.','3', '.','.','1'});
-  board.push_back(vector<char>{'7','.','.', '.','2','.', '.','.','6'});
+  board1.push_back(vector<char>{'8','.','.', '.','6','.', '.','.','3'});
+  board1.push_back(vector<char>{'4','.','.', '8','.','3', '.','.','1'});
+  board1.push_back(vector<char>{'7','.','.', '.','2','.', '.','.','6'});
 
-  board.push_back(vector<char>{'.','6','.', '.','.','.', '2','8','.'});
-  board.push_back(vector<char>{'.','.','.', '4','1','9', '.','.','5'});
-  board.push_back(vector<char>{'.','.','.', '.','8','.', '.','7','9'});
+  board1.push_back(vector<char>{'.','6','.', '.','.','.', '2','8','.'});
+  board1.push_back(vector<char>{'.','.','.', '4','1','9', '.','.','5'});
+  board1.push_back(vector<char>{'.','.','.', '.','8','.', '.','7','9'});
+
+  vector<vector<char>> board2;
+  board2.push_back(vector<char>{'.','.','9', '7','4','8', '.','.','.'});
+  board2.push_back(vector<char>{'7','.','.', '.','.','.', '.','.','.'});
+  board2.push_back(vector<char>{'.','2','.', '1','.','9', '.','.','.'});
+
+  board2.push_back(vector<char>{'.','.','7', '.','.','.', '2','4','.'});
+  board2.push_back(vector<char>{'.','6','4', '.','1','.', '5','9','.'});
+  board2.push_back(vector<char>{'.','9','8', '.','.','.', '3','.','.'});
+
+  board2.push_back(vector<char>{'.','.','.', '8','.','3', '.','2','.'});
+  board2.push_back(vector<char>{'.','.','.', '.','.','.', '.','.','6'});
+  board2.push_back(vector<char>{'.','.','.', '2','7','5', '9','.','.'});
 
   int n = 6^3;
   int* a = new int[9];
@@ -114,6 +166,9 @@ int main()
   cellbyrow[1].push_back(c);
 
   cout << "There are " << (std::bitset<9>(n)).count() << " ones in number " << n << endl;
+  cout << "This works: " << (n <<= 1) << endl;
+  cout << "This works again: " << (n <<= 1) << endl;
+
   cout << "This works: " << cellbyrow[0].size() << " " << cellbyrow[1].size() << endl;
 
   for (auto it = cellbyrow[1].begin(); it != cellbyrow[1].end(); ++it) {
@@ -124,11 +179,20 @@ int main()
   cout << "This works: " << cellbyrow[0].size() << " " << cellbyrow[1].size() << endl << endl;
   
   Solution sol;
-  sol.solveSudoku(board);
 
+  // cout << "The solution for board 1 is: " << endl;
+  // sol.solveSudoku(board1);
+  // for (int i = 0; i < 9; ++i) {
+  //   for (int j = 0; j < 9; ++j)
+  //     cout << board1[i][j] << ' ';
+  //   cout << endl;
+  // }
+
+  cout << "The solution for board 2 is: " << endl;
+  sol.solveSudoku(board2);
   for (int i = 0; i < 9; ++i) {
     for (int j = 0; j < 9; ++j)
-      cout << board[i][j] << ' ';
+      cout << board2[i][j] << ' ';
     cout << endl;
   }
 
