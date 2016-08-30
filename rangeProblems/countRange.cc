8b5e3ac3e71a9dbf8b29e140b5932c0c33704a46
wscisme <wscisme@gmail.com>
2016-08-30 00:29:02 -0700

    Count of Range Sum under development

--- rangeProblems/countRange.cc
+++ rangeProblems/countRange.cc
@@ -38,10 +38,28 @@ void mergeCountSort(vector<pair<int,int>>& array, vector<int>& sol) {
   }
 }
 
+/*
+void mergeCountSort2(int idxarr[], int lo, int hi, vector<int>& nums, vector<int>& sol) {
+  int arrsize = hi - lo;
+  if (arrsize < 2) return;
+
+  int half = arrsize/2;
+  mergeCountSort2(idxarr, lo, half, nums, sol);
+  mergeCountSort2(idxarr, half, hi, nums, sol);
+
+  // for (auto it1 = halfarr1.begin(), it2 = halfarr2.begin(), iend1 = halfarr1.end(), iend2 = halfarr2.end(); ;) {
+  for (int i = lo, j = half; ;) {
+    if (i == half) {
+      if (j == hi) break;
+    }
+  }
+}
+*/
+
 vector<int> Solution::countSmaller(vector<int>& nums) {
   const unsigned int nsize = nums.size();
   vector<int> sol(nsize);
-  
+
   vector<pair<int,int>> val_idx_pairs;
   for (unsigned int idx = 0; idx < nsize; ++idx) {
     val_idx_pairs.push_back(make_pair(nums[idx], idx));
@@ -50,9 +68,47 @@ vector<int> Solution::countSmaller(vector<int>& nums) {
 
   int i = 1;
   mergeCountSort(val_idx_pairs, sol);
-  
+
   return sol;
 }
+
+inline void mergeSortCountRangeSum(int larr[], int rarr[], int arrsize) {
+
+}
+
+int countRangeSum(vector<int>& nums, int lower, int upper, int i, int k) {
+  if (i == k) return 0;
+  int arrsize = k - i;
+  if (arrsize == 1) return nums[i] >= lower && nums[i] <= upper;
+  int half = (i+k)/2;
+  int sum = countRangeSum(nums, lower, upper, i, half) + countRangeSum(nums, lower, upper, half, j);
+  if (arrsize == 2) {
+    int tempsum = nums[i] + nums[i+1];
+    sum += (tempsum >= lower && tempsum <= upper);
+    return sum;
+  }
+
+  int larr[half-i];
+  int rarr[k-half];
+  int tempsum = 0;
+  for (int j = half - 1; j >= i; --j) {
+    tempsum += nums[j];
+    larr[j] = tempsum;
+  }
+  tempsum = 0;
+  for (int j = half; j < k; ++j) {
+    tempsum += nums[j];
+    larr[j] = tempsum;
+  }
+
+  return sum;
+}
+
+int Solution::countRangeSum(vector<int>& nums, int lower, int upper) {
+  int half = nums.size()/2;
+  return countRangeSum(nums, lower, upper, 0, half) + countRangeSum(nums, lower, upper, half, nums.size());
+}
+
 
 int main()
 {
